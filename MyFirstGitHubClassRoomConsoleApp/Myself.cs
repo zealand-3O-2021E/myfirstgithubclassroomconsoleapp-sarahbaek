caacee8768268c9eb736de88b8c96bb8f261ce0c
sarahBæk2021 <sbaek18@gmail.com>
2021-08-31 14:30:03 +0200

    Trial push

---
+++ MyFirstGitHubClassRoomConsoleApp/Myself.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+   public class Myself
+    {
+        private string _name;
+        private int _age;
+        private bool _married;
+
+        public Myself(string Name, int Age, bool Married)
+        {
+            _name = Name;
+            _age = Age;
+            _married = Married;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+        public bool Married { get => _married; set => _married = value; }
+
+      
+    }
+}
